42872af25aba032d451e41118ef667ef4b6171d2
Darcie Browning <darcie.browning@my.jcu.edu.au>
2016-05-09 11:15:04 +1000

    Changing single post template

--- wp-content/themes/barrier-reef-orchestra/inc/template-tags.php
+++ wp-content/themes/barrier-reef-orchestra/inc/template-tags.php
@@ -33,7 +33,15 @@ function barrier_reef_orchestra_posted_on() {
 		esc_html_x( 'by %s', 'post author', 'barrier-reef-orchestra' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
-
+        
+        $author = get_the_author_meta('ID');
+        if ( gbr_validate_gravatar($author)) {
+            echo '<div class="has-avatar">';
+        echo '<div class = "author-avatar">' . get_avatar($author) . '</div>';
+        } else {
+            echo '<div class="mc">';
+        }
+        
 	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
 }
@@ -120,3 +128,57 @@ function barrier_reef_orchestra_category_transient_flusher() {
 }
 add_action( 'edit_category', 'barrier_reef_orchestra_category_transient_flusher' );
 add_action( 'save_post',     'barrier_reef_orchestra_category_transient_flusher' );
+
+
+/**
+ * Utility function to check if a gravatar exists for a given email or id
+ * @param int|string|object $id_or_email A user ID,  email address, or comment object
+ * @return bool if the gravatar exists or not
+ */
+
+function gbr_validate_gravatar($id_or_email) {
+  //id or email code borrowed from wp-includes/pluggable.php
+	$email = '';
+	if ( is_numeric($id_or_email) ) {
+		$id = (int) $id_or_email;
+		$user = get_userdata($id);
+		if ( $user )
+			$email = $user->user_email;
+	} elseif ( is_object($id_or_email) ) {
+		// No avatar for pingbacks or trackbacks
+		$allowed_comment_types = apply_filters( 'get_avatar_comment_types', array( 'comment' ) );
+		if ( ! empty( $id_or_email->comment_type ) && ! in_array( $id_or_email->comment_type, (array) $allowed_comment_types ) )
+			return false;
+
+		if ( !empty($id_or_email->user_id) ) {
+			$id = (int) $id_or_email->user_id;
+			$user = get_userdata($id);
+			if ( $user)
+				$email = $user->user_email;
+		} elseif ( !empty($id_or_email->comment_author_email) ) {
+			$email = $id_or_email->comment_author_email;
+		}
+	} else {
+		$email = $id_or_email;
+	}
+
+	$hashkey = md5(strtolower(trim($email)));
+	$uri = 'http://www.gravatar.com/avatar/' . $hashkey . '?d=404';
+
+	$data = wp_cache_get($hashkey);
+	if (false === $data) {
+		$response = wp_remote_head($uri);
+		if( is_wp_error($response) ) {
+			$data = 'not200';
+		} else {
+			$data = $response['response']['code'];
+		}
+	    wp_cache_set($hashkey, $data, $group = '', $expire = 60*5);
+
+	}		
+	if ($data == '200'){
+		return true;
+	} else {
+		return false;
+	}
+}
